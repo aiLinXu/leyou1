560b655d02da6e8782e97f5ecf339431701de966
aiLinXu <2693422196@qq.com>
2019-07-20 16:42:55 +0800

    这是完成的乐优代码

--- ly-item/ly-item-service/src/main/java/com/leyou/item/mapper/CategoryMapper.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/mapper/CategoryMapper.java
@@ -2,9 +2,15 @@ package com.leyou.item.mapper;
 
 import com.leyou.common.mappers.BaseMapper;
 import com.leyou.item.entity.Category;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 /**
  * @author 虎哥
  */
+@Repository
 public interface CategoryMapper extends BaseMapper<Category> {
 }
