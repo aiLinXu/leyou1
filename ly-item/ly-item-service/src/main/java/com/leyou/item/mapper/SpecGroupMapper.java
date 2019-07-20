2ae36746b211bee4321354b7aec2d2709da8cd7a
aiLinXu <2693422196@qq.com>
2019-07-20 16:43:16 +0800

    这是完成的乐优代码

--- ly-item/ly-item-service/src/main/java/com/leyou/item/mapper/SpecGroupMapper.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/mapper/SpecGroupMapper.java
@@ -1,10 +1,12 @@
 package com.leyou.item.mapper;
 
 import com.leyou.item.entity.SpecGroup;
+import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
 
 /**
  * @author 虎哥
  */
+@Repository
 public interface SpecGroupMapper extends Mapper<SpecGroup> {
 }
