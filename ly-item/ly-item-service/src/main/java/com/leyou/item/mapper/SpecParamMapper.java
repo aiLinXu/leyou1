4bb65ad6bb0770e77e8817074ec6e1d0d630269d
aiLinXu <2693422196@qq.com>
2019-07-20 16:43:26 +0800

    这是完成的乐优代码

--- ly-item/ly-item-service/src/main/java/com/leyou/item/mapper/SpecParamMapper.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/mapper/SpecParamMapper.java
@@ -1,10 +1,12 @@
 package com.leyou.item.mapper;
 
 import com.leyou.item.entity.SpecParam;
+import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
 
 /**
  * @author 虎哥
  */
+@Repository
 public interface SpecParamMapper extends Mapper<SpecParam> {
 }
